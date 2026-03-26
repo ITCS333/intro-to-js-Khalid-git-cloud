6fecc0605f4c71544cb41ed58007551e1e201130
Khalid-git-cloud <Khalid-git-cloud@users.noreply.github.com>
2026-03-26 14:54:31 +0300

    updated index.js code

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  */
 function sum(a, b) {
   // TODO: Implement this function.
+  return a + b;
 }
 
 /**
@@ -13,6 +14,7 @@ function sum(a, b) {
  */
 function reverseString(str) {
   // TODO: Implement this function.
+  return str.split("").reverse().join("");
 }
 
 /**
@@ -21,6 +23,19 @@ function reverseString(str) {
  */
 function findLargest(numbers) {
   // TODO: Implement this function.
+  if (numbers.length === 0) {
+    return null;
+  }
+
+  let largest = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      largest = numbers[i];
+    }
+  }
+
+  return largest;
 }
 
 /**
@@ -31,6 +46,10 @@ function findLargest(numbers) {
  */
 function isPalindrome(str) {
   // TODO: Implement this function.
+  let lowerStr = str.toLowerCase();
+  let reversed = lowerStr.split("").reverse().join("");
+
+  return lowerStr === reversed;
 }
 
 /**
@@ -39,6 +58,15 @@ function isPalindrome(str) {
  */
 function filterEvenNumbers(numbers) {
   // TODO: Implement this function.
+  let result = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      result.push(numbers[i]);
+    }
+  }
+
+  return result;
 }
 
 
